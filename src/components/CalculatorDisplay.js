ac9a687f9ad1889300dd4a94acaa18e942b48c2b
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-07 22:24:12 -0400

    CalculatorDisplay truncates number if too long

--- src/components/CalculatorDisplay.js
+++ src/components/CalculatorDisplay.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function CalculatorDisplay(props) {
+export default function CalculatorDisplay({ number }) {
+  const truncatedNumber = number.length > 12 ? number.slice(0, 12) : number
   return (
     <div className='display'>
-      {props.number}
+      {truncatedNumber}
     </div>
   )
 }
